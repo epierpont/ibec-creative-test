cddd6a6c720d0da5def5072bffc386eb3fa12fa3
epierpont <epierpont@swiftcom.com>
2021-04-16 09:00:17 -0400

    Menu show/hide now working

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,14 @@
-console.log("Hello iBec");
+document.addEventListener("DOMContentLoaded", function () {
+  console.log(
+    "iBec, I really appreciate the opportunity to participate in this test!"
+  );
+
+  // Toggle more content when arrow is clicked.
+  const menuArrow = document.querySelector(".menu-arrow");
+  const moreContent = document.querySelector(".more-content");
+
+  menuArrow.addEventListener("click", () => {
+    menuArrow.classList.toggle("clicked");
+    moreContent.classList.toggle("d-none");
+  });
+});
