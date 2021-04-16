bbf2486d77c067eb8afa55814367c6497869dc48
epierpont <epierpont@swiftcom.com>
2021-04-16 11:23:05 -0400

    Random dot circles now being changed at 1s intervals

--- js/scripts.js
+++ js/scripts.js
@@ -11,4 +11,27 @@ document.addEventListener("DOMContentLoaded", function () {
     menuArrow.classList.toggle("clicked");
     moreContent.classList.toggle("d-none");
   });
+
+  // Change dot opacity with random value.
+  function changeDotOpacity(dotNum) {
+    let randomNum = Math.floor(Math.random() * 5);
+    let opacValue = "0." + randomNum;
+    document.querySelector(dotNum).style.opacity = opacValue;
+  }
+
+  // Randomly choose a dot to target.
+  function dotInterval() {
+    const dotsArray = [
+      ".dot-one",
+      ".dot-two",
+      ".dot-three",
+      ".dot-four",
+      ".dot-five",
+    ];
+    let randomDot = Math.floor(Math.random() * dotsArray.length);
+    changeDotOpacity(dotsArray[randomDot]);
+    setTimeout(dotInterval, 1000);
+  }
+
+  dotInterval();
 });
